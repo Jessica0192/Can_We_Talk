0bbd0ae734280a663e2b6bc4e15e0013f86b58c8
Suka Sun <suka.sakura@gmail.com>
2021-04-13 22:01:39 -0700

    Update chat-server to handle in parallel send and receive msg

--- chat-server/src/chat-server.c
+++ chat-server/src/chat-server.c
@@ -20,7 +20,7 @@
 #define max_num_clients 10
 void *socketThread(void *clientSocket);
 void sigIntHandler(int signal);
-typedef struct  
+typedef struct
 {
 	// the following is a requriement of UNIX/Linux
 	long type;
@@ -28,7 +28,7 @@ typedef struct
 	int IPaddress[max_num_clients];
 	char* userID[max_num_clients];
 
-	
+
 } serverThread;
 
 // global variable to keep count of the number of clients ...
@@ -41,7 +41,7 @@ static int counter;
 int main(void)
 {
 	  serv_th = (serverThread*)malloc(sizeof(serverThread));
-	 
+
 	  int                server_socket, client_socket;
 	  int                client_len;
 	  struct sockaddr_in client_addr, server_addr;
@@ -49,23 +49,23 @@ int main(void)
 	  pthread_t	     tid[3];
 	  int                whichClient;
 
-signal (SIGINT, sigIntHandler);
-numClients = 0;
-counter=0;
+    signal (SIGINT, sigIntHandler);
+    numClients = 0;
+    counter=0;
 
-	 /*
-   * obtain a socket for the server
-   */
+    /*
+    * obtain a socket for the server
+    */
 
-	  if ((server_socket = socket (AF_INET, SOCK_STREAM, 0)) < 0) 
+	  if ((server_socket = socket (AF_INET, SOCK_STREAM, 0)) < 0)
 	  {
 	    printf ("[SERVER] : socket() FAILED\n");
 	    return 1;
 	  }
 	  printf("[SERVER] : socket() successful\n");
 
-	global_serv_socket = server_socket;
-	
+    global_serv_socket = server_socket;
+
 	  /*
 	   * initialize our server address info for binding purposes
 	   */
@@ -74,18 +74,18 @@ counter=0;
 	  server_addr.sin_addr.s_addr = htonl (INADDR_ANY);
 	  server_addr.sin_port = htons (PORT);
 
-	  if (bind (server_socket, (struct sockaddr *)&server_addr, sizeof (server_addr)) < 0) 
+	  if (bind (server_socket, (struct sockaddr *)&server_addr, sizeof (server_addr)) < 0)
 	  {
 	    printf ("[SERVER] : bind() FAILED\n");
 	    close (server_socket);
 	    return 2;
 	  }
 	  printf ("[SERVER] : bind() successful\n");
-	  
+
 	  /*
 	   * start listening on the socket
 	   */
-	  if (listen (server_socket, 5) < 0) 
+	  if (listen (server_socket, 5) < 0)
 	  {
 	    printf ("[SERVER] : listen() - FAILED.\n");
 	    close (server_socket);
@@ -93,98 +93,94 @@ counter=0;
 	  }
 	  printf ("[SERVER] : listen() successful\n");
 
-	numClients = 1;
+    numClients = 1;
 
 
-	 /*
-	   * for this server, run an endless loop that will
-	   * accept incoming requests from a remote client.
-	   * the server will create a thread to handle the
-	   * request, and the parent will continue to listen for the
-	   * next request - up to 3 clients
-	   */
-	  while (numClients != 0 && numClients <= max_num_clients) 
+    /*
+    * for this server, run an endless loop that will
+    * accept incoming requests from a remote client.
+    * the server will create a thread to handle the
+    * request, and the parent will continue to listen for the
+    * next request - up to 3 clients
+    */
+	  while (numClients != 0 && numClients <= max_num_clients)
 	  {
-		printf("[SERVER] : Ready to accept()\n");
-		fflush(stdout);
-	
-		counter++;
-	
-	       /*
-		* accept a packet from the client
-		*/
-		client_len = sizeof (client_addr);
+  		printf("[SERVER] : Ready to accept()\n");
+  		fflush(stdout);
 
-		
+  		counter++;
 
-		if ((client_socket = accept (server_socket,(struct sockaddr *)&client_addr, &client_len)) < 0) 
-		{
-		  printf ("[SERVER] : accept() FAILED\n");
-		  fflush(stdout);	
-		  return 4;
-		} 
-		//struct sockaddr_in sa;
-		char str[INET_ADDRSTRLEN];
+  	  /*
+  		* accept a packet from the client
+  		*/
+  		client_len = sizeof (client_addr);
 
+  		if ((client_socket = accept (server_socket,(struct sockaddr *)&client_addr, &client_len)) < 0)
+  		{
+  		  printf ("[SERVER] : accept() FAILED\n");
+  		  fflush(stdout);
+  		  return 4;
+  		}
+  		//struct sockaddr_in sa;
+  		char str[INET_ADDRSTRLEN];
 
-		// now get it back and print it
-		inet_ntop(AF_INET, &(client_addr.sin_addr), str, INET_ADDRSTRLEN);
+  		// now get it back and print it
+  		inet_ntop(AF_INET, &(client_addr.sin_addr), str, INET_ADDRSTRLEN);
 
-		if(numClients == 1 && counter == 1)
-		{
-			numClients--;
-		}
+  		if(numClients == 1 && counter == 1)
+  		{
+  			numClients--;
+  		}
 
-		printf("%s\n", str); // prints "192.0.2.33"
-		serv_th->IPaddress[numClients]= client_socket; //new
-		cur_IP = serv_th->IPaddress[numClients]; //new added thing
+  		printf("%s\n", str); // prints "192.0.2.33"
+  		serv_th->IPaddress[numClients]= client_socket; //new
+  		cur_IP = serv_th->IPaddress[numClients]; //new added thing
 
-		numClients++;
+  		numClients++;
 
-		
-		printf("[SERVER] : received a packet from CLIENT-%02d\n", numClients);
-		fflush(stdout);	
+  		printf("[SERVER] : received a packet from CLIENT-%02d\n", numClients);
+  		fflush(stdout);
 
-		/*
-		 * rather than fork and spawn the execution of the command within a 
-		 * child task - let's take a look at something else we could do ...
-		 *
-		 * ... we'll create a thread of execution within this task to take
-		 * care of executing the task.  We'll be looking at THREADING in a 
-		 * couple modules from now ...
-		 */
-		if (pthread_create(&(tid[(numClients-1)]), NULL, socketThread, (void *)&client_socket))
-		{
-		  printf ("[SERVER] : pthread_create() FAILED\n");
-		  fflush(stdout);	
-		  return 5;
-		}
+      /*
+      * rather than fork and spawn the execution of the command within a
+      * child task - let's take a look at something else we could do ...
+      *
+      * ... we'll create a thread of execution within this task to take
+      * care of executing the task.  We'll be looking at THREADING in a
+      * couple modules from now ...
+      */
+  		if (pthread_create(&(tid[(numClients-1)]), NULL, socketThread, (void *)&client_socket))
+  		{
+  		  printf ("[SERVER] : pthread_create() FAILED\n");
+  		  fflush(stdout);
+  		  return 5;
+  		}
 
-		printf("[SERVER] : pthread_create() successful for CLIENT-%02d\n", numClients);
-		fflush(stdout);
+  		printf("[SERVER] : pthread_create() successful for CLIENT-%02d\n", numClients);
+  		fflush(stdout);
 
 	  }
 
-	printf("\n[SERVER] : Now we wait for the threads to complete ... \n");
-	for(i=0; i<2; i++)
-	{
-	if(pthread_join(tid[i], (void *)(&whichClient)))
-	{
-	   printf("[SERVER] : pthread_join() FAILED\n");
-	   return 6;
-	}
-	printf("\n[SERVER] : received QUIT command from CLIENT-%02d\n", whichClient);
-	}
+    printf("\n[SERVER] : Now we wait for the threads to complete ... \n");
+    for(i=0; i<2; i++)
+    {
+    if(pthread_join(tid[i], (void *)(&whichClient)))
+    {
+       printf("[SERVER] : pthread_join() FAILED\n");
+       return 6;
+    }
+    printf("\n[SERVER] : received QUIT command from CLIENT-%02d\n", whichClient);
+    }
 
-	printf("\n[SERVER] : All clients have returned - exiting ...\n");
-	close (server_socket);
+    printf("\n[SERVER] : All clients have returned - exiting ...\n");
+    close (server_socket);
 
-	return 0;
+    return 0;
 }
 
 
 
-// 
+//
 // Socket handler - this function is called (spawned as a thread of execution)
 //
 
@@ -205,44 +201,65 @@ void *socketThread(void *clientSocket)
   memset(buffer,0,BUFSIZ);
 
   // increment the numClients
-  int iAmClient = numClients;	// assumes that another connection from another client 
-				// hasn't been created in the meantime
+  int iAmClient = numClients;	// assumes that another connection from another client
+                              // hasn't been created in the meantime
 
   read (clSocket, buffer, BUFSIZ);
 
   parsed_userID = strtok(buffer, "/");
-printf("parsedUSERID: %s\n", parsed_userID);
+  printf("parsedUSERID: %s\n", parsed_userID);
   serv_th->userID[numClients - 1] = parsed_userID;
+
+  // TODO: From Suka -> parsed_msg is not parsed_userID, needs to be fixed
   //parsed_msg = strtok(NULL, "/");
   parsed_msg = parsed_userID;
- printf("hello function\n");
- 	 
+
+
+  // printf("hello function\n");
 
   while(strcmp(parsed_msg,">>bye<<") != 0)
   {
-	 time_t t = time(NULL);
-         struct tm tm = *localtime(&t);
+
+    if(strcmp(parsed_userID,"000") == 0)
+    {
+      printf("[Socket: %d] In coming msg query\n", clSocket);
+      memset(buffer,0,BUFSIZ);
+      memset(message,0,sizeof(message));
+      read (clSocket, buffer, BUFSIZ);
+      parsed_userID = strtok(buffer, "/");
+      continue;
+    }
+
+    time_t t = time(NULL);
+    struct tm tm = *localtime(&t);
     /* we're actually not going to execute the command - but we could if we wanted */
 
-	printf("in function\n");
-	printf("numCleints: %d\n", numClients);
-	sprintf (message, "[%d] [%s] << %s (%02d:%02d:%02d)", clSocket, parsed_userID, parsed_msg, tm.tm_hour, tm.tm_min, tm.tm_sec);
+    printf("in function\n");
+    printf("numCleints: %d\n", numClients);
+    sprintf (message, "[%d] [%s] << %s (%02d:%02d:%02d)", clSocket, parsed_userID, parsed_msg, tm.tm_hour, tm.tm_min, tm.tm_sec);
+    printf("msg=%s\n", message);
+    printf("--- before for\n");
     for (int i = 0; i < numClients; i++){
-	    if (serv_th->IPaddress[i] != clSocket){
-		printf("WRITING!!!!!! [%d]\n", clSocket);
-		   printf("[%d] [%s] << %s (%02d:%02d:%02d)\n", clSocket, parsed_userID, parsed_msg, tm.tm_hour, tm.tm_min, tm.tm_sec);
-		if(serv_th->IPaddress[i] != 0 && write (serv_th->IPaddress[i], message, strlen(message)) == -1)
-		{
-			printf("Socket write failed\n");
-		}
-	    }
-   } 
+      printf("--- before if: %d != %d \n", serv_th->IPaddress[i], clSocket);
+
+      // TODO: From Suka -> I think this if can be removed as user's own msg needs to be displayed as well.
+	    // if (serv_th->IPaddress[i] != clSocket){
+        printf("WRITING!!!!!! [%d]\n", clSocket);
+        printf("[%d] [%s] << %s (%02d:%02d:%02d)\n", clSocket, parsed_userID, parsed_msg, tm.tm_hour, tm.tm_min, tm.tm_sec);
+        if(serv_th->IPaddress[i] != 0 && write (serv_th->IPaddress[i], message, strlen(message)) == -1)
+        {
+        	printf("Socket write failed\n");
+        }
+      // }
+      printf("--- exit if\n");
+    }
+    printf("--- exit for\n");
     // clear out and get the next command and process
     memset(buffer,0,BUFSIZ);
     memset(message,0,sizeof(message));
     read (clSocket, buffer, BUFSIZ);
   }
-  
+
   // decrement the number of clients
   numClients--;
 
@@ -255,7 +272,7 @@ printf("parsedUSERID: %s\n", parsed_userID);
   timeToExit = iAmClient;
   pthread_exit((void *)(&timeToExit));
   printf ("[SERVER (Thread-%02d)] : closing socket\n", iAmClient);
-	
+
   return 0;
 }
 
@@ -271,11 +288,9 @@ printf("parsedUSERID: %s\n", parsed_userID);
 void sigIntHandler(int signal)
 {
 
-      	// disable CTRL-C support! but only 3 times max!
+	// disable CTRL-C support! but only 3 times max!
 	printf ("SIGINT IN CHAT-SERVER\n");
 
-      	close (global_serv_socket);
+	close (global_serv_socket);
 	exit(1);
 }
-
-
